e79970ef6cad3d00f440dc39e2118eb775bf073b
oRlex <orlexfed@gmail.com>
2020-07-23 16:36:30 +0300

    Slider need to refactoring

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -16,3 +16,76 @@ const handleTabs = function (event) {
 };
 
 tabs.addEventListener('click', handleTabs);
+
+// 
+// Slider
+// 
+let position = 0;
+const sliderContainer = document.querySelector('.slider-container');
+const sliderTrack = document.querySelector('.slider-track');
+const sliderItems = document.querySelectorAll('.slider-item');
+const btnPrev = document.querySelector('.arrow-left');
+const btnNext = document.querySelector('.arrow-right');
+const sliderDot = document.querySelector('.slider-dots');
+const itemWidth = document.querySelector('.slider-item').clientWidth;
+
+
+const slideLeft = function(event){
+  event.preventDefault();
+  position += itemWidth;
+
+  setPosition();
+  checkBtn();
+};
+
+const slideRight = function(event){
+  event.preventDefault();
+  position -= itemWidth;
+
+  setPosition();
+  checkBtn();
+};
+
+const setPosition = () =>{
+  sliderTrack.style.cssText = `transform: translateX(${position}px)`;
+};
+
+const checkBtn = () => {
+  if(position === 0){
+    btnPrev.disabled = true;
+  } else{
+    btnPrev.disabled = false;
+  }
+  
+  console.log(position)
+  if (position <= -(sliderItems.length -1) * itemWidth) {
+    btnNext.disabled = true;
+  } else {
+    btnNext.disabled = false;
+  }
+
+};
+checkBtn();
+
+btnPrev.addEventListener('click', slideLeft);
+btnNext.addEventListener('click', slideRight);
+
+
+
+sliderDot.addEventListener('click', function (event) {
+  event.preventDefault();
+  for (const el of this.children) {
+    el.classList.remove('active-dot');
+  }
+  event.target.classList.toggle('active-dot');
+  const newArray = Array.from(sliderItems).map((e)=> e).filter((i)=> {
+    if (i.dataset.slide === event.target.dataset.triger) {
+      console.log(i.documentOffsetLeft)
+      position -= i.offsetLeft;
+      console.log(position)
+      setPosition();
+    }
+  });
+
+
+});
